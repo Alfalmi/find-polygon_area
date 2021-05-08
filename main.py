a05a4fe02343ba5da936ea2437baf989f96d9bd2
pinooxd <pinooxd@gmail.com>
2021-05-07 17:12:58 -0700

    done

---
+++ main.py
@@ -0,0 +1,10 @@
+from math import tan, pi
+
+side = int(input("Enter the side : "))
+lenghtside = float(input("Enter the length of the sides "))
+
+area = side * (lenghtside**2) / (4 * tan(pi/side))
+
+print('The area of the polygon is : ', area)
+
+
